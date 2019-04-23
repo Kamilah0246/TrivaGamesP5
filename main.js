bd9dd309e1dfa7678e875859acbccee395a19e9c
Cespedes <0332003@students.dadeschools.net>
2019-04-23 11:41:40 -0400

    added timeout

--- main.js
+++ main.js
@@ -32,10 +32,9 @@ var question =[
 
         countdown: function(){
             game.counter--;
-            $("#counter-number").html(game.counter):
+            $("#counter-number").html(game.counter);
             if (game.counter === 0){
-                console.log("TIMES UP");
-
+                console.log("TIME UP");
                 game.timeUp()
             }
         },
@@ -44,11 +43,48 @@ var question =[
             timer = setInterval(game.countdown, 1000);
             panel.html("<h2>" + questions[this.currentQuestion].question + "<h2>");
             for(var i = 0; i < questions[this.currentQuestion].answers.length; i++)
-        
-            panel.append("<button class = 'answer-button' id = 'button' data-name =' " + 
-            questions[this.currentQuestion].answers[i] + "'>" +
-            questions[this.currentQuestion].answers[i] + "</button>");
-        }
+            {
+                panel.append("<button class = 'answer-button' id = 'button' data-name =' " + 
+                questions[this.currentQuestion].answers[i] + "'>" +
+                questions[this.currentQuestion].answers[i] + "</button>");
+            }
+        },
+
+        nextQuestion: function() {
+            game.counter = countStartNumber;
+            $("#counter-number").html(game.counter);
+            game.currentQuestion++;
+            game.loadQuestion();
+        },
+
+        timeUp: function() {
+            clearInterval(timer);
+            $("counter-number").html(game.counter);
+            panel.html("<h2>Out of Time</h2>");
+            panel.append("<h3>The correct answer was: </h3>" + questions[this.currentQuestion].correctAnswer);
+            panel.append("<img src='" + this.questions[this.currentQuestion].image);
+
+            if (game.currentQuestion === questions.length -1)
+                {
+                    setTimeout(game.results, 3*1000);
+                }
+                else 
+                {
+                    setTimeout(game.nextQuestion, 3*1000);
+                }
+        },
+
+        results: function(){
+
+            clearInterval(timer);
+            panel.html("<h2>All done, here's how you did!</h2>");
+            $("counter-number").html(game.counter);
+
+            panel.append("<h3>Correct Answers:" + game.correct + "</h3>");
+            panel.append("<h3>Incorrect Answers:" + game.incorrect + "</h3>");
+            panel.append("<h3>Unanswered:" + (questions.length = (game.correct + game.incorrect)) + "</h3>");
+            panel.append("<br><button id= 'start-over'>Start Over?</button>");
+        },
 
 
 
